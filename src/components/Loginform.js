8f8ee985fbd654966d87377072c0a2fe2e2dcb1e
Aritra <aritra1704@gmail.com>
2019-11-13 18:15:40 +0530

    Added create users infirebase.

--- src/components/Loginform.js
+++ src/components/Loginform.js
@@ -39,33 +39,34 @@ class LoginForm extends Component {
 
   render() {
     return (
-      <SafeAreaView style={{flex: 1}}>
-        <View style={{flex: 5, borderWidth: 1}}></View>
-        <View style={{flex: 90, borderWidth: 1}}>
-          <Card style={styles.statusBarBackground}>
-            <CardSection>
-              <Input
-                label="Email"
-                placeholder="email@gmail.com"
-                onChangeText={this.onEmailChange.bind(this)}
-                value={this.props.email}
-                styles={styles.userInput}
-              />
-            </CardSection>
-            <CardSection>
-              <Input
-                secureTextEntry
-                label="Password"
-                placeholder="password"
-                onChangeText={this.onPasswordChange.bind(this)}
-                value={this.props.password}
-              />
-            </CardSection>
-            {this.renderError()}
-            <CardSection>{this.renderButton()}</CardSection>
-          </Card>
-        </View>
-      </SafeAreaView>
+      <Card>
+        <CardSection>
+          <Input
+            label="Email"
+            placeholder="email@gmail.com"
+            onChangeText={this.onEmailChange.bind(this)}
+            value={this.props.email}
+            styles={styles.userInput}
+          />
+        </CardSection>
+        <CardSection>
+          <Input
+            secureTextEntry
+            label="Password"
+            placeholder="password"
+            onChangeText={this.onPasswordChange.bind(this)}
+            value={this.props.password}
+          />
+        </CardSection>
+        {this.renderError()}
+        <CardSection>{this.renderButton()}</CardSection>
+      </Card>
+      // <SafeAreaView style={{flex: 1}}>
+      //   <View style={{flex: 5, borderWidth: 1}}></View>
+      //   <View style={{flex: 90, borderWidth: 1}}>
+
+      //   </View>
+      // </SafeAreaView>
     );
   }
 }
@@ -92,8 +93,11 @@ const mapStateToProps = ({auth}) => {
   return {email, password, error, loading};
 };
 
-export default connect(mapStateToProps, {
-  emailChanged,
-  passwordChanged,
-  loginUser,
-})(LoginForm);
+export default connect(
+  mapStateToProps,
+  {
+    emailChanged,
+    passwordChanged,
+    loginUser,
+  },
+)(LoginForm);
