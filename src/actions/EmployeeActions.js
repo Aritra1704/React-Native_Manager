7d75cbb17e44b04f2a812b00495f7d91cd5f3c70
Aritra <aritra1704@gmail.com>
2019-11-14 21:12:43 +0530

    Implemented Employee list

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,5 +1,9 @@
 import firebase from 'firebase';
-import {EMPLOYEE_UPDATE, EMPLOYEE_CREATE} from './Types';
+import {
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEE_FETCH_SUCCESS,
+} from './Types';
 import {Actions} from 'react-native-router-flux';
 
 export const employeeUpdate = ({prop, value}) => {
@@ -11,7 +15,7 @@ export const employeeUpdate = ({prop, value}) => {
 
 export const employeeCreate = ({name, phone, shift}) => {
   const {currentUser} = firebase.auth();
-  return (dispatch) => {
+  return dispatch => {
     firebase
       .database()
       .ref(`/users/${currentUser.uid}/employees`)
@@ -19,6 +23,18 @@ export const employeeCreate = ({name, phone, shift}) => {
       .then(() => {
         dispatch({type: EMPLOYEE_CREATE});
         Actions.employeeList({type: 'reset'});
+      });
+  };
+};
+
+export const employeeFetch = () => {
+  const {currentUser} = firebase.auth();
+  return dispatch => {
+    firebase
+      .database()
+      .ref(`/users/${currentUser.uid}/employees`)
+      .on('value', snapshot => {
+        dispatch({type: EMPLOYEE_FETCH_SUCCESS, payload: snapshot.val()});
       });
   };
 };
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,21 +1,44 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {connect} from 'react-redux';
+import {FlatList} from 'react-native';
+import {employeeFetch} from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
+  componentWillMount() {
+    this.props.employeeFetch();
+    this.renderRow(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.renderRow(this.nextProps);
+  }
+
+  renderRow(employee) {
+    return <ListItem employee={employee} />;
+  }
+
   render() {
     return (
-      <View>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-      </View>
+      <FlatList
+        data={this.props.employees}
+        extraData={this.props.employees}
+        renderItem={({item}) => this.renderRow(item)}
+        keyExtractor={employee => employee.uid}
+      />
     );
   }
 }
 
-export default EmployeeList;
+const mapStateToProps = state => {
+  const employees = _.map(state.employees, (val, uid) => {
+    return {...val, uid};
+  });
+  return {employees};
+};
 
+export default connect(
+  mapStateToProps,
+  {employeeFetch},
+)(EmployeeList);
